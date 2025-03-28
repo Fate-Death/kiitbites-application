7ef5ec556e28287c698fd8e99ccd20e2f3f8ff21
Fate-Death <pavitrarajpoot64@gmail.com>
2025-03-28 15:38:14 +0530

    setting up routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
-
+import { Redirect, Stack } from "expo-router";
+  
 export default function RootLayout() {
-  return <Stack screenOptions={{headerShown:false}}/>;
+ //return <Stack screenOptions={{headerShown:false}}/>;
+  return(
+    <Stack screenOptions={{headerShown:false}}>
+      <Stack.Screen name="LoginScreen"/>
+    </Stack>
+  );
+  
 }
